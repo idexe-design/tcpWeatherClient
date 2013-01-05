660e1f81174d1e0487faeffab310a5a7a5c70500
Kevin Dreßler <Kevin@MacBuch.local>
2013-01-05 16:01:28 +0100

    Initial Commit

---
+++ socketpractice2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  socketpractice2
+//
+//  Created by Kevin Dreßler on 1/5/13.
+//  Copyright (c) 2013 Kevin Dreßler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
