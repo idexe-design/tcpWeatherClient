44cb6f8e8270f288a4b8f476661e5e31046d516d
idexe-design <info@idexe-design.de>
2013-01-08 16:24:51 +0100

    Update socketpractice2/main.cpp

--- socketpractice2/main.cpp
+++ socketpractice2/main.cpp
@@ -1,67 +1,91 @@
-/*
- ** client.c -- a stream socket client demo
- */
-#include <stdio.h>
-#include <stdlib.h>
+//
+//  main.cpp
+//  socketpractice2
+//
+//  Created by Johannes Schmidt on 08.01.13.
+//  Copyright (c) 2013 Kevin Dreßler. All rights reserved.
+//
+
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <arpa/inet.h> 
+#include <sys/socket.h> 
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#define PORT "3490" // the port client will be connecting to
-#define MAXDATASIZE 100 // max number of bytes we can get at once
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+#include <iostream>
+
+using namespace std;
+
+// Stringende ermitteln und Zurückgeben.
+string find_string_end(string line)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    unsigned long symbol_position;
+    symbol_position = line.find(":",0);
+    line.erase(symbol_position);
+    return line;
 }
-int main(int argc, char *argv[])
-{
-    int sockfd, numbytes;
-    char buf[MAXDATASIZE];
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
-    if (argc != 2) {
-        fprintf(stderr,"usage: client hostname\n");
-        exit(1);
+
+int main(int argc, char *argv[]) {
+    //Int-Deklaration für Client_Socket Deskripter
+    int client_socket;
+    
+    //server_address vom strukturierten Datentyp sockaddr_in
+    struct sockaddr_in server_address;
+    
+    //Char-Zeichenkette der Länge 255 für Speicher
+    char buffer[255];
+    
+    //String-Deklaration für Ein-und Ausgabe
+    string eingabe, ausgabe;
+    
+    //Bool-Deklaration für verbindung_aktiv
+    bool verbindung_aktiv = true;
+    
+    //Schleife für aktive Verbindungsdauer
+    while(verbindung_aktiv) {
+      client_socket = socket(AF_INET, SOCK_STREAM, 0);
+      if (client_socket < 0) {
+        printf("Fehler: öffnen des Client-Sockets fehlgeschlagen!");
+        return EXIT_FAILURE;
+      }
+        
+      //Server_Address initialisieren
+      server_address.sin_family = AF_INET;
+      server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+      server_address.sin_port = htons(50000);
+        
+      if (connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address)) < 0) {
+          printf("Fehler: Verbindung zum Server konnte nicht hergestellt werden!");
+      }
+      
+
+        
+        
+        cout<<"Bitte geben Sie den Namen der Stadt ein,\nfuer die Sie das Wetter wissen moechten."<<endl;
+        cout<<"Zur Auswahl stehen\nLeipzig\nStuttgart\nHamburg\nErlangen\nKonstanz"<<endl;
+        cout<<"Zum Beenden des Programm druecken Sie bitte 'Q'."<<endl;
+        cin>>eingabe;
+        
+        if(eingabe=="Q")
+        {
+            cout<<"'Q' wurde gerückt!"<<endl;
+            verbindung_aktiv = false;
+        }
+        else
+        {
+            eingabe = eingabe +":";
+            cout<<eingabe;
+            if(send(client_socket,eingabe.c_str(),eingabe.size(),0)== -1)
+            {
+                cout<<("Fehler beim Senden!\n")<<endl;
+            }
+            recv(client_socket, buffer, sizeof(buffer),0);
+            ausgabe =  buffer;
+            cout<< find_string_end(ausgabe) <<endl;
+            cout<<"\n"<<endl;
+        }
+        
+        close(client_socket);
+        
     }
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue; }
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("client: connect");
-            continue; }
-        break; }
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        return 2;
-    }
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-              s, sizeof s);
-    printf("client: connecting to %s\n", s);
-    freeaddrinfo(servinfo); // all done with this structure
-    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-        perror("recv");
-        exit(1); }
-    buf[numbytes] = '\0';
-    printf("client: received '%s'\n",buf);
-    close(sockfd);
-    return 0; }
+    return 0;
+}
